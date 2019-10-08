3c9096a31426bf40a319cf6c1d8e18c715b803e4
JohnRaven <John-B-RAven@yandex.ru>
2019-10-08 18:54:00 +0300

    feat: add function (not complete)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,108 @@
-module.exports = function zeros(expression) {
-  // your solution
-}
+/*module.exports = function zeros(expression) {
+  var str = expression;
+  var re = /(\d+)(?=!!)/g; // ------регулярные выражения      https://developer.mozilla.org/ru/docs/Web/JavaScript/Guide/Regular_Expressions
+  //--- /{(\d+)}/g  --- вытягивает все числа внутри скобок {_}
+  //--- /\{([^\}]*)\}/  
+  //--- /\*([^\!!]*)\!!/g  --- вытягивает все числа внутри скобок *_!!
+  var arrRes = [];
+  while ((m = re.exec(str)) !== null) { //----Работа с двойными факториалами
+    if (m[1] % 2 == 0) {
+      let n = m[1];
+      let res = 0;
+      while (n >= 1) {
+        n /= 10;
+        res += n;
+      }
+      arrRes.push(Math.trunc(res));
+      str = str.replace(m[1], ' ');
+    } else if (m[1] % 2 != 0) {
+      let res = 0;
+      arrRes.push(res);
+      str = str.replace(m[1], ' ');
+    }
+  }
+  var reX = /(\d+)(?=!)/g;
+  while ((m = reX.exec(str)) !== null) { //----Работа с обычными факториалами
+    let n = m[1];
+    let res = 0;
+    while (n >= 1) {
+      n /= 5;
+      res += n;
+    }
+    arrRes.push(Math.trunc(res));
+    str = str.replace(m[1], ' ');
+  }
+  
+
+  return (arrRes.reduce((a, b) => a + b, 0));
+} */
+
+/*
+let expression = '6!';
+console.log(expression);
+
+//----- поиск в строке чисел и запись их в массив http://qaru.site/questions/157629/how-to-find-a-number-in-a-string-using-javascript 
+    var exampl = /\d+/g; // эквивалентно--  new RegExp("\d+")
+    var number;              
+    var arrText = [];     
+    while ((number = exampl.exec(expression)) != null) {
+      arrText.push(number[0]);
+    }                           
+//-----
+
+//***** разложение факториала по множителям (по 5)
+    var res = 0;
+    for (let i=0; i<=arrText.length ;i++) {
+      let n = arrText[i];
+      while (n >= 1) {
+        n /= 5;
+        res += n;
+      }
+    }
+    console.log(Math.trunc(res));   // целая часть от числа
+    
+//*****
+*/
+
+
+
+
+    var str = '9!!*10!!*7!!';
+    var re = /(\d+)(?=!!)/g;  // ------регулярные выражения      https://developer.mozilla.org/ru/docs/Web/JavaScript/Guide/Regular_Expressions
+          //--- /{(\d+)}/g  --- вытягивает все числа внутри скобок {_}
+          //--- /\{([^\}]*)\}/  
+          //--- /\*([^\!!]*)\!!/g  --- вытягивает все числа внутри скобок *_!!
+    var arrRes = [];
+    while ((m = re.exec(str)) !== null) {  //----Работа с двойными факториалами
+      if (m[1] % 2 == 0) {
+        let n = m[1];
+        let res = 0;
+        while (n >= 1) {
+          n /= 10;
+          res += n;
+        }
+        arrRes.push(Math.trunc(res));
+        str = str.replace(m[1], ' ');
+      } else if (m[1] % 2 != 0) {
+        let res = 0;
+        arrRes.push(res);
+        str = str.replace(m[1], ' ');
+      }
+    }
+    var reX = /(\d+)(?=!)/g;
+    while ((m = reX.exec(str)) !== null) { //----Работа с обычными факториалами
+        let n = m[1];
+        let res = 0;
+        while (n >= 1) {
+          n /= 5;
+          res += n;
+        }
+        arrRes.push(Math.trunc(res));
+        str = str.replace(m[1], ' ');
+    }
+    console.log(arrRes.reduce((a, b) => a + b, 0))
+
+
+// console.log(arrText);
+// console.log(arrRes, str);
+
